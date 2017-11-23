86ad160e041b27344f84f2bb97a00eda32da5325
lightning95 <aydar.gizatullin@gmail.com>
2017-11-23 23:46:45 +0300

    added expr-parser

--- hw3/src/Expr.hs
+++ hw3/src/Expr.hs
@@ -24,7 +24,7 @@ import qualified Data.Map.Strict            as Map (Map, insert, lookup)
 
 type Ident = String
 
-data Expr = Const Int
+data Expr = Const Integer
           | Var Ident
           | Sum Expr Expr
           | Sub Expr Expr
@@ -34,10 +34,10 @@ data Expr = Const Int
           | Loc Ident Expr Expr
           deriving (Show, Eq)
 
-type Bindings = Map.Map String Int
+type Bindings = Map.Map String Integer
 
 data EvalError = DivisionByZero
-               | PowerToNegative Int
+               | PowerToNegative Integer
                | UnknownIdentifier String
                deriving (Show, Eq)
 
@@ -50,14 +50,14 @@ eval :: forall m .
         , MonadReader Bindings m
         , MonadError EvalError m
         )
-     => Expr -> m Int
+     => Expr -> m Integer
 eval (Const x) = pure x
 eval (Sum x y) = (+) <$> eval x <*> eval y
 eval (Sub x y) = (-) <$> eval x <*> eval y
 eval (Mul x y) = (*) <$> eval x <*> eval y
 eval (Div x y) = div <$> eval x <*> right
   where
-    right :: m Int
+    right :: m Integer
     right = do
       res <- eval y
       if res == 0 then
--- hw3/src/ExprParser.hs
+++ hw3/src/ExprParser.hs
@@ -1,10 +1,89 @@
+{-# LANGUAGE ExplicitForAll             #-}
+{-# LANGUAGE FlexibleContexts           #-}
+{-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE PartialTypeSignatures      #-}
+{-# LANGUAGE ScopedTypeVariables        #-}
+{-# LANGUAGE TypeSynonymInstances       #-}
+
 module ExprParser where
 
-import Expr (Expr(..))
-import Control.Monad (void)
-import Data.Void
-import Text.Megaparsec
-import Text.Megaparsec.Char
-import Text.Megaparsec.Expr
-import qualified Text.Megaparsec.Lexer as L
+import           Control.Applicative      (empty)
+import           Control.Monad            (void)
+import           Data.Void                (Void)
+import           Expr                     (Expr (..), Ident)
+import           Text.Megaparsec
+import           Text.Megaparsec.Char     
+import           Text.Megaparsec.Expr
+import qualified Text.Megaparsec.Char.Lexer    as L
 
+import           Control.Monad.State.Lazy
+
+type Parser = Parsec Void String
+
+sc :: Parser ()
+sc = L.space space empty empty
+
+lexeme :: Parser a -> Parser a
+lexeme = L.lexeme sc
+
+symbol :: String -> Parser String
+symbol = L.symbol sc
+
+parens :: Parser a -> Parser a
+parens = between (symbol "(") (symbol ")")
+ 
+integer :: Parser Integer
+integer = lexeme L.decimal
+
+rword :: String -> Parser ()
+rword w = lexeme (string w *> notFollowedBy alphaNumChar)
+
+rws :: [String] -- list of reserved words
+rws = ["let", "in"]
+
+identifier :: Parser Ident
+identifier = (lexeme . try) (p >>= check)
+  where
+    p       = (:) <$> letterChar <*> many alphaNumChar
+    check x = if x `elem` rws
+                then fail $ "keyword " ++ show x ++ " cannot be an identifier"
+                else return x
+
+exprParser :: Parser Expr
+exprParser = between sc eof expr
+
+expr :: Parser Expr
+expr = parens exprParser
+  <|> parens locParser
+  <|> binOpParser
+  <|> constParser
+  <|> varParser
+
+locParser :: Parser Expr
+locParser = do
+  rword "let"
+  ident <- identifier
+  void (symbol "=")
+  val <- exprParser
+  rword "in"
+  right <- exprParser
+  pure $ Loc ident val right
+
+constParser :: Parser Expr
+constParser = Const <$> integer
+
+varParser :: Parser Expr
+varParser = Var <$> identifier
+
+binOpParser :: Parser Expr
+binOpParser = makeExprParser exprParser aOperators
+
+aOperators :: [[Operator Parser Expr]]
+aOperators = [ [ InfixR (Pow <$ symbol "^")]
+             , [ InfixL (Mul <$ symbol "*")
+               , InfixL (Div <$ symbol "/")]
+             , [ InfixL (Sum <$ symbol "+")
+               , InfixL (Sub <$ symbol "-")]
+             ]
