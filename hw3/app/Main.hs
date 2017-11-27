9f6393e1014d1e685c302b604098a64fa38cbb13
lightning95 <aydar.gizatullin@gmail.com>
2017-11-27 18:31:30 +0300

    kEK

--- hw3/app/Main.hs
+++ hw3/app/Main.hs
@@ -1,4 +1,26 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import           Data.Either        (rights)
+import           Data.Text          (Text, pack, splitOn, unpack)
+import           InterpreterIO      (interpretIO)
+import           System.Environment (getArgs)
+import           Text.Megaparsec    (parse)
+import           VarsParser         (Assign, assignParser)
+
 main :: IO ()
-main = undefined
+main = do
+  args <- getArgs
+  file <- readFile $ head args
+  let
+    strs :: [Text]
+    strs = splitOn "\n" $ pack file
+    unpacked :: [String]
+    unpacked = filter (not . null) $ map unpack strs
+    linez :: [Assign]
+    linez = rights $ map (parse assignParser "") unpacked
+  res <- interpretIO linez
+  case res of
+    Left e       -> print e
+    Right (_, b) -> print b
